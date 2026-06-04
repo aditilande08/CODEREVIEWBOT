df57648ec2531e3360df7c4e488cc20d4fc1cc3d
Aditi Lande <aditilande02@gmail.com>
2026-06-05 04:23:24 +0530

    fix CORS credentials bug

--- backend/server.py
+++ backend/server.py
@@ -378,8 +378,9 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000", "http://127.0.0.1:5173"],
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
     allow_methods=["*"],
     allow_headers=["*"],
+)
 )
